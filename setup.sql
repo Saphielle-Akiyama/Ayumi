156e912137458f67526f5ece5fcf9e687c100551
Moogs <saphielle.akiyama@gmail.com>
2020-08-03 12:58:37 +0200

    Modified row names

--- setup.sql
+++ setup.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS anime_reminders (
     user_id bigint NOT NULL,
     trigger_time timestamp with time zone NOT NULL,
     anime_name text NOT NULL,
-    message_url text NOT NULL
+    channel_id bigint NOT NULL
     );
 
